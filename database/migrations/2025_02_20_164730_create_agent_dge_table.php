45f7495496e1a2ca293d16da7fb7ff7b3c0a112d
Soxna Rama Mu Sell Mi <ramatoulayef56@gmail.com>
2025-03-13 10:54:55 +0000

    ajout de la base de donnée

--- database/migrations/2025_02_20_164730_create_agent_dge_table.php
+++ database/migrations/2025_02_20_164730_create_agent_dge_table.php
@@ -6,30 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('agent_dge', function (Blueprint $table) {
             $table->id();
             $table->string('nom_utilisateur')->unique();
             $table->string('password');
-            $table->string('userable_type');
-            $table->unsignedBigInteger('userable_id');
+            $table->string('nom');
+            $table->string('prenom');
             $table->timestamp('date_creation');
-            $table->rememberToken();
             $table->timestamps();
-
-            $table->index(['userable_type', 'userable_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('agent_dge');
     }
 };
