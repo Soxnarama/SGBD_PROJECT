5d93232ffc16c7a1db907a2bd4c18825f3bdd00b
sokhna-sall <sallsokhna2003@gmail.com>
2025-03-13 12:16:35 +0100

    creation de Models et gestion de mail

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
@@ -18,9 +20,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'nom_utilisateur',
         'password',
+        'userable_type',
+        'userable_id',
+        'date_creation',
     ];
 
     /**
@@ -39,7 +43,46 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'date_creation' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Get the parent userable model (AgentDGE, Candidat, ou Parrain).
+     */
+    public function userable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * Determine if the user can access the Filament admin panel.
+     */
+    public function canAccessFilamentPanel(Panel $panel): bool
+    {
+        return $this->userable_type === 'AgentDGE';
+    }
+
+    /**
+     * Determine if the user can access the Filament admin panel.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // Temporairement permettre à tous les utilisateurs d'accéder
+    }
+
+
+    // une fonction role qui retourne le role de l'utilisateur, candidat, agentde ou parrain
+    public function role()
+    {
+        // return $this->userable_type;
+        if ($this->userable_type === 'App\Models\AgentDGE') {
+            return 'agentdge';
+        } elseif ($this->userable_type === 'App\Models\Candidat') {
+            return 'candidat';
+        } elseif ($this->userable_type === 'App\Models\Parrain') {
+            return 'parrain';
+        }
+    }
+
 }
